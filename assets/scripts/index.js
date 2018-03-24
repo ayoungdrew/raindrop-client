481febabad51cf39c90d127e704361aee3184a56
ptavarez16 <ptavarez16@yahoo.com>
2018-03-24 17:50:30 -0400

    Add product click handler - Add variable productEvents to require product's events.js file - Add function that connects event handler to html

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,12 @@
 'use strict'
 
 const authEvents = require('./auth/events.js')
+const productEvents = require('./products/events.js')
 
 $(() => {
   authEvents.addHandlers()
+})
+
+$(() => {
+  productEvents.productHandlers()
 })
