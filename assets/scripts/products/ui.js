8d69118c81c0df8be02dd6b8409bf06fb0ecf3fc
ptavarez16 <ptavarez16@yahoo.com>
2018-03-24 18:26:05 -0400

    Add handlebars template to render all products
    products/ui.js:
      - Add showProductsTemplate variable to require
      product-listing.handlebars file
      - Add showProducts variable within getProductsSuccess function
      - Add jQuery function that invokes handlebar
    
    product-listing.handlebars
     - Add template to populate #all-products-content div

--- assets/scripts/products/ui.js
+++ assets/scripts/products/ui.js
@@ -1,7 +1,12 @@
 'use strict'
 
+const showProductsTemplate = require('../templates/product-listing.handlebars')
+
 const getProductsSuccess = function (data) {
   console.log(data)
+
+  const showProducts = showProductsTemplate({ products: data.products })
+  $('#all-products-content').html(showProducts)
 }
 
 const getProductsFailure = function () {
