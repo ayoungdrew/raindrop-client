4bed50adacef0d1c57a8c72b3a62c88510ee8f64
ptavarez16 <ptavarez16@yahoo.com>
2018-03-24 14:29:22 -0400

    Text removal - Remove unnecessary text from index.js file

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,13 +5,3 @@ const authEvents = require('./auth/events.js')
 $(() => {
   authEvents.addHandlers()
 })
-
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-
-// use require without a reference to ensure a file is bundled
-// require('./example')
-
-$(() => {
-  // your JS code goes here
-})
