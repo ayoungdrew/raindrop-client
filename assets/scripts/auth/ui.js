ebcaee602d7042c3ddf3d00450d3ddb3a8d1f33a
ptavarez16 <ptavarez16@yahoo.com>
2018-03-27 11:54:41 -0400

    Callback modification - Updated toast callback function - Now adding message parameter to be passed onto the toast function

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,24 +1,27 @@
 'use strict'
 
 const store = require('../store')
-const toast = require('../toasts/ui.js')
+const toast = require('../templates/toast.js')
 
 const signUpSuccess = function (data) {
   $('#sign-up-modal').modal('hide')
   $('#sign-up').get(0).reset()
-  toast.signUpSuccess()
+  toast.success('Account successfully created!')
   console.log('Sign Up Success')
 }
 
 const signUpFailure = function () {
   $('#sign-up').get(0).reset()
+  toast.failure('There was an error on creating your account. Please try again.')
   console.log('Sign Up Error')
 }
 
 const signInSuccess = function (data) {
   store.user = data.user
   console.log('User data is: ', data.user)
+  $('#sign-in-modal').modal('hide')
   $('#sign-in').get(0).reset()
+  toast.success('Successfully signed in.')
   console.log('Sign In Success')
   $('#intro-header').css({
     'display': 'block'
@@ -28,21 +31,25 @@ const signInSuccess = function (data) {
 const signInFailure = function (error) {
   $('#sign-in').get(0).reset()
   console.error(error)
+  toast.failure('There was an error on your sign in attempt. Please try again.')
   console.log('Sign In Error')
 }
 
 const changePasswordSuccess = function (data) {
   $('#change-password-modal').modal('hide')
   $('#change-password').get(0).reset()
+  toast.success('Successfully changed password.')
   console.log('Change Password Success')
 }
 
 const changePasswordFailure = function () {
   $('#change-password').get(0).reset()
+  toast.failure('There was an error on your change password attempt. Please try again.')
   console.log('Change Password Error')
 }
 
 const signOutSuccess = function (data) {
+  toast.success('Successfully signed out.')
   console.log('Sign Out Success')
   $('#sign-out').css({
     'display': 'none'
@@ -53,6 +60,7 @@ const signOutSuccess = function (data) {
 }
 
 const signOutFailure = function () {
+  toast.failure('There was an error on your sign out attempt. Please try again.')
   console.log('Sign Out Error')
 }
 
